34dacbb51e11d89bb7bdd8f42606fd8133e15ea6
david <david@david-laptop>
2007-12-23 04:15:55 +0900

    Put NumpyCheckLibAndHeader in numscons top namespace.

--- numscons/__init__.py
+++ numscons/__init__.py
@@ -1,3 +1,4 @@
 from core.misc import get_scons_path
 from core.numpyenv import GetNumpyEnvironment
+from core.libinfo_scons import NumpyCheckLibAndHeader
 import tools
--- numscons/setup.py
+++ numscons/setup.py
@@ -3,6 +3,7 @@
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('numscons',parent_package,top_path)
+    config.add_subpackage('checkers')
     config.add_subpackage('core')
     config.add_subpackage('tools')
     return config
