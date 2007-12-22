66bd07bb963ba5838b4d55afb448533146213542
david <david@david-laptop>
2007-12-23 03:09:27 +0900

    Cleaning up misc module

--- numscons/core/misc.py
+++ numscons/core/misc.py
@@ -1,9 +1,16 @@
-from os.path import join, dirname
+"""Misc utilities which did not find they way elsewhere."""
+
+import sys
+import os
+import re
+
+from os.path import join as pjoin, dirname as pdirname, basename as pbasename
 
 import numscons
 
 def get_scons_path():
-    return join(dirname(numscons.__file__), 'scons-local')
+    """Returns the name of the directory where our local scons is."""
+    return pjoin(pdirname(numscons.__file__), 'scons-local')
 
 # Those built_* are not good: we should have a better way to get the real type
 # of compiler instead of being based on names (to support things like colorgcc,
@@ -34,6 +41,7 @@ def get_pythonlib_name(debug = 0):
 		       (sys.hexversion >> 16) & 0xff)
 
 def pyplat2sconsplat():
+    """Returns the scons platform."""
     # XXX: should see how env['PLATFORM'] is defined, make this a dictionary 
     if sys.platform[:5] == 'linux':
         return 'posix'
@@ -54,17 +62,21 @@ def is_cc_suncc(fullpath):
     cmd = fullpath + ' -V 2>&1'
     out = os.popen(cmd)
     cnt = out.read()
-    st = out.close()
+    #st = out.close()
+    out.close()
 
     return suncc.search(cnt)
 
 def get_local_toolpaths():
-    return [os.path.dirname(numpy.distutils.scons.tools.__file__)]
+    """Returns the full pathname for the numscons tools directory."""
+    return [pdirname(numscons.tools.__file__)]
 
 def get_custom_toolpaths(env):
+    """Returns the list of user-customized pathnames for scons tools."""
     return env['scons_tool_path'].split(os.pathsep)
 
 def get_additional_toolpaths(env):
+    """Returns the full list of pathnames where to look for scons tools."""
     toolp = []
     # Put custom toolpath FIRST !
     toolp.extend(get_custom_toolpaths(env))
@@ -72,10 +84,13 @@ def get_additional_toolpaths(env):
     return toolp
 
 def is_f77_gnu(fullpath):
+    """Returns true if the command given by fullpath is a Gnu fortran 
+    compiler."""
     # XXX: do this properly
     return pbasename(fullpath) == 'g77' or pbasename(fullpath) == 'gfortran'
 
 def get_vs_version(env):
+    """Returns the visual studio version."""
     try:
         version = env['MSVS']['VERSION']
         m = re.compile("([0-9]).([0-9])").match(version)
@@ -86,5 +101,5 @@ def get_vs_version(env):
         else:
             raise RuntimeError("FIXME: failed to parse VS version")
     except KeyError:
-	    raise RuntimeError("Could not get VS version !")
+        raise RuntimeError("Could not get VS version !")
 
