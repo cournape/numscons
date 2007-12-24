e8222e28b23956cf67f115c950fb10687e72ef60
david <david@david-laptop>
2007-12-24 16:33:06 +0900

    Finish cleaning extension.py

--- numscons/core/extension.py
+++ numscons/core/extension.py
@@ -1,17 +1,14 @@
 #! /usr/bin/env python
 # Last Change: Tue Oct 09 03:00 PM 2007 J
 
-# Module for support to build python extension. KEEP THIS INDEPENDANT OF SCONS
+"""Module to support building python extension. KEEP THIS INDEPENDANT OF SCONS
+!"""
 
-# XXX: we should avoid clashing between distutils and numpy.distutils. Maybe by
-# defining our own functions here, and accessing any info related to python
-# build option and co from here only.
 import sys
 import os
 from os.path import join as pjoin
 
-from distutils.sysconfig import get_python_version, get_python_inc, \
-                                get_config_vars
+from distutils.sysconfig import get_python_version, get_python_inc
 
 def get_pythonlib_dir():
     """Returns a list of path to look for the python engine library
