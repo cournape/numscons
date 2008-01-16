6f815b6ec377f14a188eaa477bc5a7220eb684ad
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-01-16 20:43:03 +0900

    Put sunperf klduge in separate module

---
+++ numscons/checkers/misc.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python
+# Last Change: Wed Jan 16 08:00 PM 2008 J
+from os.path import join as pjoin, basename, dirname
+
+from numscons.testcode_snippets import cblas_sgemm
+from numscons.core.utils import popen_wrapper
+
+from support import save_and_set, restore
+
+def get_sunperf_link_options(context, res):
+    """If successefull, returns the link flags.
+    
+    Returns:
+        - st: int
+            0 : failed
+            1 : succeeded
+        - flags: dict
+            dictionary of the options.
+    """
+    context.Message('Getting link options of sunperf ... ')
+
+    opts = res.cfgopts
+    test_code = cblas_sgemm
+    env = context.env
+    saved = save_and_set(env, opts)
+    try:
+        st = context.TryCompile(test_code, '.c')
+    finally:
+        restore(env, saved)
+
+    if not st:
+        return context.Result('Failed !'), None
+
+    saved = save_and_set(env, opts)
+    env.Append(LINKFLAGS = '-#')
+    oldLINK = env['LINK']
+    env['LINK'] = '$CC'
+    try:
+        # XXX: does this scheme to get the program name always work ? Can
+        # we use Scons to get the target name from the object name ?
+        slast = str(context.lastTarget)
+        prdir = dirname(slast)
+        test_prog = pjoin(prdir, basename(slast).split('.')[0])
+    
+        cmd = context.env.subst('$LINKCOM', 
+            		    target = context.env.File(test_prog),
+            		    source = context.lastTarget)
+        st, out = popen_wrapper(cmd, merge = True)
+    finally:
+        restore(env, saved)
+        env['LINK'] = oldLINK
+
+    # If getting the verbose output succeeds, parse the output
+    if not st:
+        return 1, floupi(out)
+    else:
+        return 0, None
+
+haha = r"""
+cc -o build/scons/numpy/scons_fake/checkers/.sconf/conftest_5 -xlic_lib=sunperf -# build/scons/numpy/scons_fake/checkers/.sconf/conftest_5.o
+### Note: NLSPATH = /opt/SUNWspro/prod/bin/../lib/locale/%L/LC_MESSAGES/%N.cat:/opt/SUNWspro/prod/bin/../../lib/locale/%L/LC_MESSAGES/%N.cat
+###     command line files and options (expanded):
+	### -xlic_lib=sunperf build/scons/numpy/scons_fake/checkers/.sconf/conftest_5.o -o build/scons/numpy/scons_fake/checkers/.sconf/conftest_5
+	### Note: LD_LIBRARY_PATH = <null>
+	### Note: LD_RUN_PATH = <null>
+	/usr/ccs/bin/ld /opt/SUNWspro/prod/lib/crti.o /opt/SUNWspro/prod/lib/crt1.o /opt/SUNWspro/prod/lib/values-xa.o -o build/scons/numpy/scons_fake/checkers/.sconf/conftest_5 -lsunperf -lfui -lfsu -lmtsk -lsunmath -lpicl -lm build/scons/numpy/scons_fake/checkers/.sconf/conftest_5.o -Y "P,/opt/SUNWspro/lib:/opt/SUNWspro/prod/lib:/usr/ccs/lib:/lib:/usr/lib" -Qy -R/opt/SUNWspro/lib -lc /opt/SUNWspro/prod/lib/crtn.o"""
+
+def floupi(out):
+    import shlex
+    import os
+    lexer = shlex.shlex(out, posix = True)
+    lexer.whitespace_split = True
+
+    accept_libs = ['sunperf', 'fui', 'fsu', 'mtsk', 'sunmath']
+    keep = dict(zip(['libs', 'library_dirs', 'rpath'], [[], [], []]))
+    t = lexer.get_token()
+    while t:
+        def parse(token):
+            if token.startswith('-l'):
+                n = token.split('-l')[1]
+                if n in accept_libs:
+                    keep['libs'].append(n)
+                t = lexer.get_token()
+            elif token.startswith('-Y'):
+                n = token
+                t = lexer.get_token()
+                if t.startswith('P,'):
+                    t = t[2:]
+                nt = t.split(os.pathsep)
+                keep['library_dirs'].extend(nt)
+            elif token.startswith('-Qy'):
+                n = token
+                t = lexer.get_token()
+                if t.startswith('-R'):
+                    arg = t.split('-R', 1)[1]
+                    nt = arg.split(os.pathsep)
+                    keep['rpath'].extend(nt)
+            else:
+                t = lexer.get_token()
+            return t
+        t = parse(t)
+
+    return keep
+
--- numscons/checkers/perflib.py
+++ numscons/checkers/perflib.py
@@ -9,19 +9,13 @@ library specific check if possible, or other heuristics.
 Generally, you don't use those directly: they are used in 'meta' checkers, such
 as BLAS, CBLAS, LAPACK checkers."""
 
-import re
 from copy import deepcopy
-from os.path import join as pjoin
-from os.path import basename, dirname
 
-from support import save_and_set, restore
 from configuration import ConfigRes, add_perflib_info
 from common import check_code as _check
 from perflib_config import IsFactory, GetVersionFactory, CONFIG
 from version_checkers import atlas_version_checker, mkl_version_checker
-
-from numscons.core.utils import popen_wrapper
-from numscons.testcode_snippets import cblas_sgemm
+from misc import get_sunperf_link_options
 
 #--------------
 # MKL checker
@@ -110,103 +104,22 @@ def CheckSunperf(context, autoadd = 1, check_version = 0):
     if not st:
         return st, res
 
-    # We are not done: the option -xlic_lib=sunperf is not used by the linked
+    # We are not done: the option -xlic_lib=sunperf is not used by the linker
     # for shared libraries, I have no idea why. So if the test is succesfull,
     # we need more work to get the link options necessary to make the damn
     # thing work.
-    context.Message('Getting link options of sunperf ... ')
-
-    opts = res.cfgopts
-    test_code = cblas_sgemm
-    env = context.env
-    saved = save_and_set(env, opts)
-    try:
-        st = context.TryCompile(test_code, '.c')
-    finally:
-        restore(env, saved)
-
-    if not res:
-        return context.Result('Failed !'), res
-
-    saved = save_and_set(env, opts)
-    env.Append(LINKFLAGS = '-#')
-    oldLINK = env['LINK']
-    env['LINK'] = '$CC'
-    try:
-        # XXX: does this scheme to get the program name always work ? Can
-        # we use Scons to get the target name from the object name ?
-        slast = str(context.lastTarget)
-        prdir = dirname(slast)
-        test_prog = pjoin(prdir, basename(slast).split('.')[0])
-    
-        cmd = context.env.subst('$LINKCOM', 
-            		    target = context.env.File(test_prog),
-            		    source = context.lastTarget)
-        st, out = popen_wrapper(cmd, merge = True)
-    finally:
-        restore(env, saved)
-        env['LINK'] = oldLINK
-    
-    # If the verbose output succeeds, parse the output
-    if not st:
-        st = 1
-        pa = floupi(out)
-        for k, v in pa.items():
+    st, flags = get_sunperf_link_options(context, res)
+    if st:
+        opts = res.cfgopts
+        for k, v in flags.items():
             opts[k].extend(deepcopy(v))
         res = ConfigRes(cfg.name, opts, res.is_customized())
         add_perflib_info(context.env, 'Sunperf', res)
         context.Result('Succeeded !')
     else:
-        st = 0
         context.Result('Failed !')
 
     return st, res
-
-haha = r"""
-cc -o build/scons/numpy/scons_fake/checkers/.sconf/conftest_5 -xlic_lib=sunperf -# build/scons/numpy/scons_fake/checkers/.sconf/conftest_5.o
-### Note: NLSPATH = /opt/SUNWspro/prod/bin/../lib/locale/%L/LC_MESSAGES/%N.cat:/opt/SUNWspro/prod/bin/../../lib/locale/%L/LC_MESSAGES/%N.cat
-###     command line files and options (expanded):
-	### -xlic_lib=sunperf build/scons/numpy/scons_fake/checkers/.sconf/conftest_5.o -o build/scons/numpy/scons_fake/checkers/.sconf/conftest_5
-	### Note: LD_LIBRARY_PATH = <null>
-	### Note: LD_RUN_PATH = <null>
-	/usr/ccs/bin/ld /opt/SUNWspro/prod/lib/crti.o /opt/SUNWspro/prod/lib/crt1.o /opt/SUNWspro/prod/lib/values-xa.o -o build/scons/numpy/scons_fake/checkers/.sconf/conftest_5 -lsunperf -lfui -lfsu -lmtsk -lsunmath -lpicl -lm build/scons/numpy/scons_fake/checkers/.sconf/conftest_5.o -Y "P,/opt/SUNWspro/lib:/opt/SUNWspro/prod/lib:/usr/ccs/lib:/lib:/usr/lib" -Qy -R/opt/SUNWspro/lib -lc /opt/SUNWspro/prod/lib/crtn.o"""
-
-def floupi(out):
-    import shlex
-    import os
-    lexer = shlex.shlex(out, posix = True)
-    lexer.whitespace_split = True
-
-    accept_libs = ['sunperf', 'fui', 'fsu', 'mtsk', 'sunmath']
-    keep = dict(zip(['libs', 'library_dirs', 'rpath'], [[], [], []]))
-    t = lexer.get_token()
-    while t:
-        def parse(token):
-            if token.startswith('-l'):
-                n = token.split('-l')[1]
-                if n in accept_libs:
-                    keep['libs'].append(n)
-                t = lexer.get_token()
-            elif token.startswith('-Y'):
-                n = token
-                t = lexer.get_token()
-                if t.startswith('P,'):
-                    t = t[2:]
-                nt = t.split(os.pathsep)
-                keep['library_dirs'].extend(nt)
-            elif token.startswith('-Qy'):
-                n = token
-                t = lexer.get_token()
-                if t.startswith('-R'):
-                    arg = t.split('-R', 1)[1]
-                    nt = arg.split(os.pathsep)
-                    keep['rpath'].extend(nt)
-            else:
-                t = lexer.get_token()
-            return t
-        t = parse(t)
-
-    return keep
 
 IsSunperf = IsFactory('Sunperf').get_func()
 
