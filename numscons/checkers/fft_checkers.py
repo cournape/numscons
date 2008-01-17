ef0ef025b286e5a766e917e6cfa9377fa5d2ac48
david <david@david-laptop>
2008-01-17 21:05:06 +0900

    Fix remaining typo related to checkers revamp in fft_checker

--- numscons/checkers/fft_checkers.py
+++ numscons/checkers/fft_checkers.py
@@ -27,21 +27,24 @@ def CheckFFT(context, autoadd = 1, check_version = 0):
     def check_fft_perflib(perflibs):
         def check(pname):
             func = checker(pname)
-            st = func(context, autoadd, check_version)
-            # XXX: check for fft code ?
-            if st:
+            if func(context, autoadd, check_version):
+                # XXX: check for fft code ?
                 cache = get_cached_perflib_info(context.env, pname)
                 cfgopts = cache.opts_factory[libname]()
                 add_lib_info(env, libname, MetalibInfo(pname, cfgopts))
+                st = 1
+            else:
+                st = 0
 
             return st
+
         for p in perflibs:
             if check(p):
                 return 1
         return 0
 
     if check_fft_perflib(('MKL', 'FFTW3', 'FFTW2')):
-        return st
+        return 1
 
     add_lib_info(env, libname, None)
     return 0
