e0dea225b06ae20a22bd83022745320c45e25d80
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-01-25 17:47:33 +0900

    Add test for fortran checkers, imported from old numpy.scons branch

---
+++ numscons/checkers/fortran/tests/test_fortran.py
@@ -0,0 +1,41 @@
+#! Last Change: Fri Jan 25 05:00 PM 2008 J
+import unittest
+
+from numscons.checkers.fortran.fortran import parse_f77link
+
+from fortran_output import g77_link_output, gfortran_link_output, \
+        sunfort_v12_link_output, ifort_v10_link_output, \
+        mingw_g77_link_output, \
+        g77_link_expected, gfortran_link_expected, \
+        sunfort_v12_link_expected, ifort_v10_link_expected
+
+class test_CheckF77Verbose(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_g77(self):
+        """Parsing g77 link output."""
+        assert parse_f77link(g77_link_output.split('\n')) == g77_link_expected
+
+    def test_gfortran(self):
+        """Parsing gfortran link output."""
+        assert parse_f77link(gfortran_link_output.split('\n')) == \
+               gfortran_link_expected
+
+    def test_sunf77(self):
+        """Parsing sunfort link output."""
+        assert parse_f77link(sunfort_v12_link_output.split('\n')) == \
+               sunfort_v12_link_expected
+
+    def test_intel_posix(self):
+        """Parsing ifort link output."""
+        assert parse_f77link(ifort_v10_link_output.split('\n')) == \
+               ifort_v10_link_expected
+
+    def test_intel_win(self):
+        """Parsing ifort link output on win32."""
+        print "FIXME: testing verbose output of win32 intel fortran"
+
+    def test_mingw_g77(self):
+        """Parsing mingw g77 link output on win32 (native, i.e. no cygwin)"""
+        print parse_f77link(mingw_g77_link_output.split('\n'))
