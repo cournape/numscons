38b62f893e2569f7a5a865a139915a2ab0cf9118
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-01-26 15:59:05 +0900

    Start 0.3.3dev

--- release.py
+++ release.py
@@ -13,7 +13,7 @@ Operating System :: MacOS
 """
 
 NAME        = 'numscons'
-VERSION     = '0.3.2'
+VERSION     = '0.3.3dev'
 DESCRIPTION = 'Enable to use scons within distutils to build extensions'
 CLASSIFIERS = filter(None, CLASSIFIERS.split('\n'))
 AUTHOR      = 'David Cournapeau'
