7976093bfb92796ea7641f581640ceeb08d58a4c
root <root@DAVID>
2008-02-19 01:03:21 +0900

    Forgot to import unique from utils.

--- numscons/checkers/fortran/fortran_scons.py
+++ numscons/checkers/fortran/fortran_scons.py
@@ -103,9 +103,10 @@ def CheckF77Clib(context):
         final_flags = parse_f77link(cnt)
         if built_with_mstools(env) and built_with_gnu_f77(env):
             from fortran import gnu_to_scons_flags, find_libs_paths
+	    from numscons.core.utils import unique
             pf = gnu_to_scons_flags(final_flags)
             pf["LIBS"] = unique(pf["LIBS"])
-            find_libs_paths(pf["LIBS"], pf["LIBPATH"])
+            print find_libs_paths(pf["LIBS"], pf["LIBPATH"])
             context.Result('Failed !')
             return 0
         env['F77_LDFLAGS'] = final_flags
