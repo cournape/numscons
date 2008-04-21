7eb96f959b8feea25c8637f93f60da133c71eb30
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-04-21 18:56:59 +0900

    Starting working on 0.6.2 .

--- release.py
+++ release.py
@@ -13,7 +13,7 @@ Operating System :: MacOS
 """
 
 NAME        = 'numscons'
-VERSION     = '0.6.1'
+VERSION     = '0.6.2dev'
 DESCRIPTION = 'Enable to use scons within distutils to build extensions'
 CLASSIFIERS = filter(None, CLASSIFIERS.split('\n'))
 AUTHOR      = 'David Cournapeau'
