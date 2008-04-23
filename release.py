f475a035cf3ac99ce3bdeea50e7eba84dbef5fcf
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-04-23 10:45:56 +0900

    Start working on 0.7 branch.

--- release.py
+++ release.py
@@ -13,7 +13,7 @@ Operating System :: MacOS
 """
 
 NAME        = 'numscons'
-VERSION     = '0.6.3.1'
+VERSION     = '0.7dev'
 DESCRIPTION = 'Enable to use scons within distutils to build extensions'
 CLASSIFIERS = filter(None, CLASSIFIERS.split('\n'))
 AUTHOR      = 'David Cournapeau'
