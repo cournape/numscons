5f7ae319c7d6aad7b8e3942950413a9cda14cccf
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-06-09 15:39:24 +0900

    Fix space issue in PYEXTLINKCOM.

--- numscons/tools/pyext.py
+++ numscons/tools/pyext.py
@@ -112,7 +112,7 @@ def set_basic_vars(env):
         env['PYEXTLINKCOM'] = '${TEMPFILE("$PYEXTLINK $PYEXTLINKFLAGS '\
                               '/OUT:$TARGET.windows $SOURCES.windows")}'
     else:
-        env['PYEXTLINKCOM'] = "$PYEXTLINK -o $TARGET $PYEXTLINKFLAGS $SOURCES"\
+        env['PYEXTLINKCOM'] = "$PYEXTLINK -o $TARGET $PYEXTLINKFLAGS $SOURCES "\
                             "$_LIBDIRFLAGS $_LIBFLAGS"
 
 def _set_configuration_nodistutils(env):
