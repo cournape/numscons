6426529f4ae578265dca2565f7ba5711d076ba8f
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-06-12 14:41:08 +0900

    Adapting build dir since we are now using the simpler build_dir option of SConscript.

--- numscons/core/numpyenv.py
+++ numscons/core/numpyenv.py
@@ -16,8 +16,8 @@ class NumpyEnvironment(Environment):
             # XXX handle this gracefully
             assert 0 == 1
         else:
-            kw['conf_dir'] = pjoin(self['build_dir'], '.sconf')
-            kw['log_file'] = pjoin(self['build_dir'], 'config.log')
+            kw['conf_dir'] = 'sconf'
+            kw['log_file'] = 'config.log'
         return Environment.Configure(self, *args, **kw)
 
     def Tool(self, toolname, path = None):
@@ -27,3 +27,8 @@ class NumpyEnvironment(Environment):
                     path + get_numscons_toolpaths(self))
         else:
             return Environment.Tool(self, toolname, get_numscons_toolpaths(self))
+
+    def DistutilsSConscript(self, name):
+        """This sets up build directory correctly to play nice with
+        distutils."""
+        Environment.SConscript(self, pjoin('$build_dir', '$src_dir', name))
