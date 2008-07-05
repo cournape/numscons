380709fd6bf06b47a3d432edd31a02c0982ae523
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-07-05 16:31:47 +0900

    Fix tab/space.

--- numscons/core/allow_undefined.py
+++ numscons/core/allow_undefined.py
@@ -3,23 +3,22 @@ import os
 from subprocess import Popen, PIPE
 
 def get_darwin_version():
-
-	p = Popen(["sw_vers", "-productVersion"], stdout = PIPE, stderr = PIPE)
-	st = p.wait()
-	if st:
-		raise RuntimeError(
+    p = Popen(["sw_vers", "-productVersion"], stdout = PIPE, stderr = PIPE)
+    st = p.wait()
+    if st:
+        raise RuntimeError(
                 "Could not execute sw_vers -productVersion to get version")
 
-	verstring = p.stdout.next()
-	a, b, c = verstring.split(".")
-	try:
-		major = int(a)
-		minor = int(b)
-		micro = int(c)
+    verstring = p.stdout.next()
+    a, b, c = verstring.split(".")
+    try:
+        major = int(a)
+        minor = int(b)
+        micro = int(c)
 
-		return major, minor, micro
-	except ValueError:
-		raise ValueError("Could not parse version string %s" % verstring)
+        return major, minor, micro
+    except ValueError:
+        raise ValueError("Could not parse version string %s" % verstring)
 
 def get_darwin_allow_undefined():
     """Return the list of flags to allow undefined symbols in a shared library.
