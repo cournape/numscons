203ecd1a3b2d2e542ce5f0a1cc1f6c58a4fc87a1
david <david@evans.local>
2008-07-08 14:02:26 +0200

    Add an example for numpyext.

---
+++ tests/examples/numpyext/setup.py
@@ -0,0 +1,14 @@
+import os
+import os.path
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('pyext',parent_package,top_path)
+
+    config.add_sconscript('SConstruct', source_files = ['hellomodule.c'])
+    config.add_data_dir('tests')
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(configuration=configuration)
