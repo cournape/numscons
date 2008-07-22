bb40da7437ff6788f0e443d4c99d2b439c259886
david <david@evans>
2008-07-22 19:25:44 +0200

    Start working on reliable compiler detection from command path. Add basic tests.

---
+++ numscons/core/compiler_detection.py
@@ -0,0 +1,93 @@
+import re
+
+from numscons.core.utils import popen_wrapper
+
+GNUCC = re.compile('gcc version ([0-9-.]+)')
+SUNCC = re.compile('Sun C ([0-9-.]+)')
+SUNCXX = re.compile('Sun C\+\+ ([0-9-.]+)')
+SUNFC = re.compile('Sun Fortran 95 ([0-9-.]+)')
+
+def parse_suncc(string):
+    m = SUNCC.search(string)
+    if m:
+        return True, m.group(1)
+    else:
+        return False, None
+
+def parse_suncxx(string):
+    m = SUNCXX.search(string)
+    if m:
+        return True, m.group(1)
+    else:
+        return False, None
+
+def parse_sunfortran(string):
+    m = SUNFC.search(string)
+    if m:
+        return True, m.group(1)
+    else:
+        return False, None
+
+def parse_gnu(string):
+    m = GNUCC.search(string)
+    if m:
+        return True, m.group(1)
+    else:
+        return False, None
+
+def is_suncc(path):
+    """Return True if the compiler in path is sun C compiler."""
+    # If the Sun compiler is not given a file as an argument, it returns an
+    # error code, even when using dry run and version. So we give a non
+    # existing file as an argument: this seems to work, at least for Sun Studio
+    # 12 (5.9)
+    cmd = [path, '-V', "-###", "nonexistingfile.fakec"]
+    st, cnt = popen_wrapper(cmd, merge = True)
+    ret = parse_suncc(cnt)
+    if st == 0 and ret:
+        return ret
+    else:
+        return False, None
+
+def is_suncxx(path):
+    """Return True if the compiler in path is sun CXX compiler."""
+    # Works on 5.9 (Sun Studio 12). Note that the C++ compiler works in a
+    # sensible manner when given -V compared to the C compiler...
+    cmd = [path, '-V']
+    st, cnt = popen_wrapper(cmd, merge = True)
+    ret = parse_suncxx(cnt)
+    if st == 0 and ret:
+        return ret
+    else:
+        return False, None
+
+def is_sunfortran(path):
+    """Return True if the compiler in path is sun fortran compiler."""
+    # Works on 8.3 (Sun Studio 12)
+    cmd = [path, '-V']
+    st, cnt = popen_wrapper(cmd, merge = True)
+    ret = parse_sunfortran(cnt)
+    if st == 0 and ret:
+        return ret
+    else:
+        return False, None
+
+def is_gcc(path):
+    """Return True if the compiler in path is GNU compiler."""
+    cmd = [path, '-v']
+    st, cnt = popen_wrapper(cmd, merge = True)
+    ret = parse_gnu(cnt)
+    if st == 0 and ret:
+        return ret
+    else:
+        return False, None
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        raise ValueError("Usage: %s compiler" % __file__)
+    cc = sys.argv[1]
+    print "Is Sun C: ", is_suncc(cc)
+    print "Is Sun CXX: ", is_suncxx(cc)
+    print "Is Sun Fortran: ", is_sunfortran(cc)
+    print "Is Gnu : ", is_gcc(cc)
---
+++ numscons/core/tests/test_compiler_detection.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# Last Change: Fri Nov 16 01:00 PM 2007 J
+
+# test module for utils module
+import os
+import unittest
+
+from numscons.core.compiler_detection import *
+
+GCC_4_2_3 = """
+Using built-in specs.
+Target: i486-linux-gnu
+Configured with: ../src/configure -v --enable-languages=c,c++,fortran,objc,obj-c++,treelang --prefix=/usr --enable-shared --with-system-zlib --libexecdir=/usr/lib --without-included-gettext --enable-threads=posix --enable-nls --with-gxx-include-dir=/usr/include/c++/4.2 --program-suffix=-4.2 --enable-clocale=gnu --enable-libstdcxx-debug --enable-objc-gc --enable-mpfr --enable-targets=all --enable-checking=release --build=i486-linux-gnu --host=i486-linux-gnu --target=i486-linux-gnu
+Thread model: posix
+gcc version 4.2.3 (Ubuntu 4.2.3-2ubuntu7)"""
+
+SUNCC_12 = """
+cc: Sun C 5.9 Linux_i386 Patch 124871-01 2007/07/31
+### Note: NLSPATH = /home/david/opt/sunstudio12/prod/bin/../lib/locale/%L/LC_MESSAGES/%N.cat:/home/david/opt/sunstudio12/prod/bin/../../lib/locale/%L/LC_MESSAGES/%N.cat
+###     command line files and options (expanded):
+### -V main.c
+/home/david/opt/sunstudio12/prod/bin/acomp -xldscope=global -i main.c -y-fbe -y/home/david/opt/sunstudio12/prod/bin/fbe -y-xarch=generic -y-o -ymain.o -y-verbose -y-xthreadvar=no%dynamic -y-comdat -xdbggen=no%stabs+dwarf2+usedonly -V -xdbggen=incl -y-s -m32 -fparam_ir -Qy -D__SUNPRO_C=0x590 -D__unix -D__unix__ -D__i386 -D__i386__ -D__linux__ -D__linux -Dlinux -D__gnu__linux__ -D__BUILTIN_VA_ARG_INCR -D__C99FEATURES__ -Xa -D__PRAGMA_REDEFINE_EXTNAME -Dunix -Di386 -D__RESTRICT -xc99=%all,no%lib -D__FLT_EVAL_METHOD__=-1 -c99OS -I/home/david/opt/sunstudio12/prod/include/cc "-g/home/david/opt/sunstudio12/prod/bin/cc -V -c " -fsimple=0 -D__SUN_PREFETCH -destination_ir=yabe
+### Note: LD_LIBRARY_PATH = /home/david/opt/sunstudio12/lib:/home/david/opt/sunstudio12/rtlibs:
+### Note: LD_RUN_PATH = <null>
+/usr/bin/ld -m elf_i386 -dynamic-linker /lib/ld-linux.so.2 --enable-new-dtags /home/david/opt/sunstudio12/prod/lib/crti.o /home/david/opt/sunstudio12/prod/lib/crt1.o /home/david/opt/sunstudio12/prod/lib/values-xa.o -V main.o -Y "/home/david/opt/sunstudio12/prod/lib:/lib:/usr/lib" -Qy -lc /home/david/opt/sunstudio12/prod/lib/libc_supp.a /home/david/opt/sunstudio12/prod/lib/crtn.o
+"""
+
+SUNCXX_12 = """sunCC: Sun C++ 5.9 Linux_i386 Patch 124865-01 2007/07/30"""
+
+SUNFORTRAN_12 = """
+NOTICE: Invoking /home/david/opt/sunstudio12/bin/f90 -f77 -ftrap=%none -V
+f90: Sun Fortran 95 8.3 Linux_i386 Patch 127145-01 2007/07/31
+Usage: f90 [ options ] files.  Use 'f90 -flags' for details"""
+
+class OutputParser(unittest.TestCase):
+    def test_gnu(self):
+        ret = parse_gnu(GCC_4_2_3)
+        assert ret == (True, "4.2.3")
+
+    def test_suncc(self):
+        ret = parse_suncc(SUNCC_12)
+        assert ret == (True, "5.9")
+
+    def test_suncxx(self):
+        ret = parse_suncxx(SUNCXX_12)
+        assert ret == (True, "5.9")
+
+    def test_sunfortran(self):
+        ret = parse_sunfortran(SUNFORTRAN_12)
+        assert ret == (True, "8.3")
+
+if __name__ == "__main__":
+    unittest.main()
