bdbcdc037bd3d0ddc091a6a11d4c0a43f23fef25
david <david@evans>
2008-07-24 04:07:24 +0900

    Refactor compiler version/type parsers/detection.

--- numscons/core/compiler_detection.py
+++ numscons/core/compiler_detection.py
@@ -10,45 +10,38 @@ SUNCC = re.compile('Sun C ([0-9-.]+)')
 SUNCXX = re.compile('Sun C\+\+ ([0-9-.]+)')
 SUNFC = re.compile('Sun Fortran 95 ([0-9-.]+)')
 
-def parse_icc(string):
-    m = ICC.search(string)
+def _parse(regex, string):
+    m = regex.search(string)
     if m:
         return True, m.group(1)
     else:
         return False, None
+
+def parse_icc(string):
+    return _parse(ICC, string)
 
 def parse_ifort(string):
-    m = IFORT.search(string)
-    if m:
-        return True, m.group(1)
-    else:
-        return False, None
+    return _parse(IFORT, string)
 
 def parse_suncc(string):
-    m = SUNCC.search(string)
-    if m:
-        return True, m.group(1)
-    else:
-        return False, None
+    return _parse(SUNCC, string)
 
 def parse_suncxx(string):
-    m = SUNCXX.search(string)
-    if m:
-        return True, m.group(1)
-    else:
-        return False, None
+    return _parse(SUNCXX, string)
 
 def parse_sunfortran(string):
-    m = SUNFC.search(string)
-    if m:
-        return True, m.group(1)
-    else:
-        return False, None
+    return _parse(SUNFC, string)
 
 def parse_gnu(string):
-    m = GNUCC.search(string)
-    if m:
-        return True, m.group(1)
+    return _parse(GNUCC, string)
+
+def _is_compiler(path, cmdargs, parser):
+    # cmdargs is a list of arguments to get verbose information
+    cmd = [path] + cmdargs
+    st, cnt = popen_wrapper(cmd, merge = True, shell = False)
+    ret, ver = parser(cnt)
+    if st == 0 and ret:
+        return ret, ver
     else:
         return False, None
 
@@ -58,66 +51,30 @@ def is_suncc(path):
     # error code, even when using dry run and version. So we give a non
     # existing file as an argument: this seems to work, at least for Sun Studio
     # 12 (5.9)
-    cmd = [path, '-V', "-###", "nonexistingfile.fakec"]
-    st, cnt = popen_wrapper(cmd, merge = True, shell = False)
-    ret, ver = parse_suncc(cnt)
-    if st == 0 and ret:
-        return ret, ver
-    else:
-        return False, None
+    return _is_compiler(path, ['-V', "-###", "nonexistingfile.fakec"], 
+                        parse_suncc)
 
 def is_suncxx(path):
     """Return True if the compiler in path is sun CXX compiler."""
-    # Works on 5.9 (Sun Studio 12). Note that the C++ compiler works in a
+    # Note that the C++ compiler works in a
     # sensible manner when given -V compared to the C compiler...
-    cmd = [path, '-V']
-    st, cnt = popen_wrapper(cmd, merge = True, shell = False)
-    ret, ver = parse_suncxx(cnt)
-    if st == 0 and ret:
-        return ret, ver
-    else:
-        return False, None
+    return _is_compiler(path, ['-V'], parse_suncxx)
 
 def is_sunfortran(path):
     """Return True if the compiler in path is sun fortran compiler."""
-    # Works on 8.3 (Sun Studio 12)
-    cmd = [path, '-V']
-    st, cnt = popen_wrapper(cmd, merge = True, shell = False)
-    ret, ver = parse_sunfortran(cnt)
-    if st == 0 and ret:
-        return ret, ver
-    else:
-        return False, None
+    return _is_compiler(path, ['-V'], parse_sunfortran)
 
 def is_icc(path):
     """Return True if the compiler in path is Intel C compiler."""
-    cmd = [path, '-V']
-    st, cnt = popen_wrapper(cmd, merge = True, shell = False)
-    ret, ver = parse_icc(cnt)
-    if st == 0 and ret:
-        return ret, ver
-    else:
-        return False, None
+    return _is_compiler(path, ['-V'], parse_icc)
 
 def is_ifort(path):
     """Return True if the compiler in path is Intel Fortran compiler."""
-    cmd = [path, '-V']
-    st, cnt = popen_wrapper(cmd, merge = True, shell = False)
-    ret, ver = parse_ifort(cnt)
-    if st == 0 and ret:
-        return ret, ver
-    else:
-        return False, None
+    return _is_compiler(path, ['-V'], parse_ifort)
 
 def is_gcc(path):
     """Return True if the compiler in path is GNU compiler."""
-    cmd = [path, '-v']
-    st, cnt = popen_wrapper(cmd, merge = True, shell = False)
-    ret, ver = parse_gnu(cnt)
-    if st == 0 and ret:
-        return ret, ver
-    else:
-        return False, None
+    return _is_compiler(path, ['-v'], parse_gnu)
 
 def get_cc_type(path):
     if is_gcc(path)[0]:
