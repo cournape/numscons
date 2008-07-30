e35c2a27aca351360274f255fb1ce22a083d5a11
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2008-07-30 13:04:10 +0900

    Put tools initialization in separate module.

--- numscons/core/helpers.py
+++ numscons/core/helpers.py
@@ -7,22 +7,17 @@ import sys
 import os
 from os.path import join as pjoin, basename
 
-from numscons.core.default import tool_list
-from numscons.core.compiler_config import get_config as get_compiler_config, \
-     NoCompilerConfig, CompilerConfig
 from numscons.core.custom_builders import DistutilsSharedLibrary, NumpyCtypes, \
      DistutilsPythonExtension, DistutilsStaticExtLibrary, NumpyPythonExtension
+from numscons.corfe.initialization import initialize_tools
 from numscons.core.siteconfig import get_config
 from numscons.core.extension_scons import createStaticExtLibraryBuilder
 from numscons.core.extension import get_pythonlib_dir
 from numscons.core.utils import pkg_to_path, flatten
-from numscons.core.misc import pyplat2sconsplat, \
-     get_numscons_toolpaths, iscplusplus, get_pythonlib_name, \
-     is_f77_gnu, get_vs_version, built_with_mstools, cc_version, \
+from numscons.core.misc import get_numscons_toolpaths, get_pythonlib_name, \
+     is_f77_gnu, get_vs_version, built_with_mstools, \
      isfortran, isf2py, scons_get_paths, get_scons_build_dir, \
      get_scons_configres_filename, built_with_mingw
-from numscons.core.compiler_detection import get_cc_type, get_f77_type, \
-     get_cxx_type
 
 from numscons.core.template_generators import generate_from_c_template, \
      generate_from_f_template, generate_from_template_emitter, \
@@ -33,9 +28,6 @@ from numscons.tools.substinfile import TOOL_SUBST
 from numscons.numdist import msvc_runtime_library
 
 __all__ = ['GetNumpyEnvironment', 'GetInitEnvironment']
-
-DEF_LINKERS, DEF_C_COMPILERS, DEF_CXX_COMPILERS, DEF_ASSEMBLERS, \
-DEF_FORTRAN_COMPILERS, DEF_ARS, DEF_OTHER_TOOLS = tool_list(pyplat2sconsplat())
 
 def GetNumpyOptions(args):
     """Call this with args=ARGUMENTS to take into account command line args."""
@@ -84,22 +76,6 @@ def GetNumpyOptions(args):
                         "true if bootrapping numpy, false if not", 0))
 
     return opts
-
-def customize_compiler(name, env, lang):
-    """Customize env options related to the given tool and language."""
-    # XXX: we have to fix how to detect compilers/version instead of hack after
-    # hack...
-    if lang == "C":
-        ver = cc_version(env)
-        if ver:
-            name = '%s%1.0f' % (name, ver)
-
-    try:
-        cfg = get_compiler_config(name, lang)
-    except NoCompilerConfig, e:
-        print "%s => using default configuration." % e
-        cfg = CompilerConfig()
-    env["NUMPY_CUSTOMIZATION"][lang] = cfg
 
 def finalize_env(env):
     """Call this at the really end of the numpy environment initialization."""
@@ -176,110 +152,8 @@ def GetNumpyEnvironment(args):
 
     return env
 
-def initialize_cc(env, path_list):
-    """Initialize C compiler from distutils info."""
-    from SCons.Tool import Tool, FindTool
-
-    def set_cc_from_distutils():
-        # XXX: To keep backward compatibility with numpy 1.1.1. Can be dropped at
-        # 1.1.2
-        if env["cc_opt"] == "intelc":
-            env["cc_opt"] = "icc"
-
-        if len(env['cc_opt_path']) > 0:
-            if built_with_mstools(env):
-                t = Tool("msvc", toolpath = get_numscons_toolpaths(env))
-                # We need msvs tool too (before customization !)
-                Tool('msvs')(env)
-                path_list.insert(0, env['cc_opt_path'])
-            else:
-                cc = get_cc_type(pjoin(env['cc_opt_path'], env['cc_opt']))
-                if cc == 'gcc' and sys.platform == 'win32':
-                    cc = 'mingw'
-                t = Tool(cc, toolpath = get_numscons_toolpaths(env))
-                path_list.insert(0, env['cc_opt_path'])
-        else:
-            # Do not care about PATH info because none given from scons
-            # distutils command
-            try:
-                t = Tool(env['cc_opt'], toolpath = get_numscons_toolpaths(env))
-            except ImportError:
-                raise UnknownCompiler(env['cc_opt'])
-
-        return t
-
-    if len(env['cc_opt']) > 0:
-        t = set_cc_from_distutils()
-    else:
-        t = Tool(FindTool(DEF_C_COMPILERS, env),
-                 toolpath = get_numscons_toolpaths(env))
-    t(env)
-
-    customize_compiler(t.name, env, "C")
-
 def has_f77(env):
     return len(env['f77_opt']) > 0
-
-def initialize_f77(env, path_list):
-    """Initialize F77 compiler from distutils info."""
-    from SCons.Tool import Tool, FindTool
-
-    def set_f77_from_distutils():
-        t = None
-        env.AppendUnique(F77FILESUFFIXES = ['.f'])
-        if len(env['f77_opt']) > 0:
-            if len(env['f77_opt_path']) > 0:
-                f77 = pjoin(env['f77_opt_path'], env['f77_opt'])
-                t = Tool(get_f77_type(f77),
-                         toolpath = get_numscons_toolpaths(env))
-                path_list.insert(0, env['f77_opt_path'])
-            else:
-                t = Tool(get_f77_type(env["f77_opt"]),
-                         toolpath = get_numscons_toolpaths(env))
-        else:
-            def_fcompiler =  FindTool(DEF_FORTRAN_COMPILERS, env)
-            if def_fcompiler:
-                t = Tool(def_fcompiler, toolpath = get_numscons_toolpaths(env))
-
-        return t
-
-    t = set_f77_from_distutils()
-    if t:
-        t(env)
-        customize_compiler(t.name, env, "F77")
-
-def initialize_cxx(env, path_list):
-    """Initialize C++ compiler from distutils info."""
-    from SCons.Tool import Tool, FindTool
-
-    def set_cxx_from_distutils():
-        t = None
-        if len(env['cxx_opt']) > 0:
-            if len(env['cxx_opt_path']) > 0:
-                if built_with_mstools(env):
-                    t = Tool("msvc", toolpath = get_numscons_toolpaths(env))
-                    # We need msvs tool too (before customization !)
-                else:
-                    cxx = get_cxx_type(pjoin(env['cxx_opt_path'], env['cxx_opt']))
-                    t = Tool(cxx, toolpath = get_numscons_toolpaths(env))
-                    path_list.insert(0, env['cxx_opt_path'])
-        else:
-            def_cxxcompiler =  FindTool(DEF_CXX_COMPILERS, env)
-            if def_cxxcompiler:
-                t = Tool(def_cxxcompiler, toolpath = get_numscons_toolpaths(env))
-        return t
-
-    t = set_cxx_from_distutils()
-    if t:
-        t(env)
-        customize_compiler(t.name, env, "CXX")
-        env['CXXFILESUFFIX'] = '.cxx'
-    else:
-        # Some scons tools initialize CXX env var even if no CXX available.
-        # This is just confusing, so remove the key here since we could not
-        # initialize any CXX tool.
-        if env.has_key('CXX'):
-            del env['CXX']
 
 def set_bootstrap(env):
     import __builtin__
@@ -450,64 +324,6 @@ def add_custom_builders(env):
 
     createStaticExtLibraryBuilder(env)
     env['BUILDERS']['DistutilsStaticExtLibrary'] = DistutilsStaticExtLibrary
-
-# Our own smart_link: the one in scons is not that smart.
-def dumb_link(source, target, env, for_signature):
-    has_cplusplus = iscplusplus(source)
-    # XXX: it would be nice to raise a warning if F77_LDFLAGS is not found in
-    # env and we link some fortran code.
-    if has_cplusplus:
-        return '$CXX'
-    return '$CC'
-
-def initialize_allow_undefined(env):
-    import SCons
-    from allow_undefined import get_darwin_allow_undefined
-
-    if sys.platform == 'darwin':
-        env['ALLOW_UNDEFINED'] = get_darwin_allow_undefined()
-    else:
-        env['ALLOW_UNDEFINED'] = SCons.Util.CLVar('')
-
-def initialize_tools(env):
-    from SCons.Tool import Tool, FindTool
-
-    # List of supplemental paths to take into account
-    path_list = []
-
-    # Initialize CC tool from distutils info
-    initialize_cc(env, path_list)
-
-    # Initialize F77 tool from distutils info
-    initialize_f77(env, path_list)
-
-    # Initialize CXX tool from distutils info
-    initialize_cxx(env, path_list)
-
-    # Adding default tools for the one we do not customize: mingw is special
-    # according to scons, don't ask me why, but this does not work as expected
-    # for this tool.
-    if not built_with_mingw(env):
-        for i in [DEF_LINKERS, DEF_ASSEMBLERS, DEF_ARS]:
-            t = FindTool(i, env) or i[0]
-            Tool(t)(env)
-    else:
-        try:
-            t = FindTool(['g++'], env)
-            #env['LINK'] = None
-        except EnvironmentError:
-            raise RuntimeError('g++ not found: this is necessary with mingw32 '\
-                               'to build numpy !')
-        # XXX: is this really the right place ?
-        env.AppendUnique(CFLAGS = '-mno-cygwin')
-
-    # Set ALLOW_UNDEFINED link flags to allow undefined symbols in dynamic
-    # libraries
-    initialize_allow_undefined(env)
-
-    # Use our own stupid link, because we handle this with conf checkers and
-    # link flags.
-    env['SMARTLINK']   = dumb_link
 
 def customize_tools(env):
     customize_pyext(env)
---
+++ numscons/core/initialization.py
@@ -0,0 +1,197 @@
+"""This module initialize compilers, linkers, etc... from informations passed
+from distutils, and using scons tools.
+
+The main function is initialize_tools: this is the top function which
+initialize all the tools."""
+
+from os.path import join as pjoin
+import sys
+
+from numscons.core.misc import built_with_mstools, built_with_mingw, \
+    get_numscons_toolpaths, pyplat2sconsplat, cc_version, iscplusplus
+from numscons.core.compiler_detection import get_cc_type, get_f77_type, \
+    get_cxx_type
+from numscons.core.compiler_config import get_config as get_compiler_config, \
+    NoCompilerConfig, CompilerConfig
+from numscons.core.default import tool_list
+from numscons.core.allow_undefined import get_darwin_allow_undefined
+
+DEF_LINKERS, DEF_C_COMPILERS, DEF_CXX_COMPILERS, DEF_ASSEMBLERS, \
+DEF_FORTRAN_COMPILERS, DEF_ARS, DEF_OTHER_TOOLS = tool_list(pyplat2sconsplat())
+
+def customize_compiler(name, env, lang):
+    """Customize env options related to the given tool and language."""
+    # XXX: we have to fix how to detect compilers/version instead of hack after
+    # hack...
+    if lang == "C":
+        ver = cc_version(env)
+        if ver:
+            name = '%s%1.0f' % (name, ver)
+
+    try:
+        cfg = get_compiler_config(name, lang)
+    except NoCompilerConfig, e:
+        print "%s => using default configuration." % e
+        cfg = CompilerConfig()
+    env["NUMPY_CUSTOMIZATION"][lang] = cfg
+
+def initialize_cc(env, path_list):
+    """Initialize C compiler from distutils info."""
+    from SCons.Tool import Tool, FindTool
+
+    def set_cc_from_distutils():
+        # XXX: To keep backward compatibility with numpy 1.1.1. Can be dropped
+        # at 1.1.2
+        if env["cc_opt"] == "intelc":
+            env["cc_opt"] = "icc"
+
+        if len(env['cc_opt_path']) > 0:
+            if built_with_mstools(env):
+                t = Tool("msvc", toolpath = get_numscons_toolpaths(env))
+                # We need msvs tool too (before customization !)
+                Tool('msvs')(env)
+                path_list.insert(0, env['cc_opt_path'])
+            else:
+                cc = get_cc_type(pjoin(env['cc_opt_path'], env['cc_opt']))
+                if cc == 'gcc' and sys.platform == 'win32':
+                    cc = 'mingw'
+                t = Tool(cc, toolpath = get_numscons_toolpaths(env))
+                path_list.insert(0, env['cc_opt_path'])
+        else:
+            # Do not care about PATH info because none given from scons
+            # distutils command
+            try:
+                t = Tool(env['cc_opt'], toolpath = get_numscons_toolpaths(env))
+            except ImportError:
+                raise UnknownCompiler(env['cc_opt'])
+
+        return t
+
+    if len(env['cc_opt']) > 0:
+        t = set_cc_from_distutils()
+    else:
+        t = Tool(FindTool(DEF_C_COMPILERS, env),
+                 toolpath = get_numscons_toolpaths(env))
+    t(env)
+
+    customize_compiler(t.name, env, "C")
+
+def initialize_f77(env, path_list):
+    """Initialize F77 compiler from distutils info."""
+    from SCons.Tool import Tool, FindTool
+
+    def set_f77_from_distutils():
+        t = None
+        env.AppendUnique(F77FILESUFFIXES = ['.f'])
+        if len(env['f77_opt']) > 0:
+            if len(env['f77_opt_path']) > 0:
+                f77 = pjoin(env['f77_opt_path'], env['f77_opt'])
+                t = Tool(get_f77_type(f77),
+                         toolpath = get_numscons_toolpaths(env))
+                path_list.insert(0, env['f77_opt_path'])
+            else:
+                t = Tool(get_f77_type(env["f77_opt"]),
+                         toolpath = get_numscons_toolpaths(env))
+        else:
+            def_fcompiler =  FindTool(DEF_FORTRAN_COMPILERS, env)
+            if def_fcompiler:
+                t = Tool(def_fcompiler, toolpath = get_numscons_toolpaths(env))
+
+        return t
+
+    t = set_f77_from_distutils()
+    if t:
+        t(env)
+        customize_compiler(t.name, env, "F77")
+
+def initialize_cxx(env, path_list):
+    """Initialize C++ compiler from distutils info."""
+    from SCons.Tool import Tool, FindTool
+
+    def set_cxx_from_distutils():
+        t = None
+        if len(env['cxx_opt']) > 0:
+            if len(env['cxx_opt_path']) > 0:
+                if built_with_mstools(env):
+                    t = Tool("msvc", toolpath = get_numscons_toolpaths(env))
+                    # We need msvs tool too (before customization !)
+                else:
+                    cxx = get_cxx_type(pjoin(env['cxx_opt_path'],
+                                             env['cxx_opt']))
+                    t = Tool(cxx, toolpath = get_numscons_toolpaths(env))
+                    path_list.insert(0, env['cxx_opt_path'])
+        else:
+            def_cxxcompiler =  FindTool(DEF_CXX_COMPILERS, env)
+            if def_cxxcompiler:
+                t = Tool(def_cxxcompiler,
+                         toolpath = get_numscons_toolpaths(env))
+        return t
+
+    t = set_cxx_from_distutils()
+    if t:
+        t(env)
+        customize_compiler(t.name, env, "CXX")
+        env['CXXFILESUFFIX'] = '.cxx'
+    else:
+        # Some scons tools initialize CXX env var even if no CXX available.
+        # This is just confusing, so remove the key here since we could not
+        # initialize any CXX tool.
+        if env.has_key('CXX'):
+            del env['CXX']
+
+def initialize_tools(env):
+    from SCons.Tool import Tool, FindTool
+
+    # List of supplemental paths to take into account
+    path_list = []
+
+    # Initialize CC tool from distutils info
+    initialize_cc(env, path_list)
+
+    # Initialize F77 tool from distutils info
+    initialize_f77(env, path_list)
+
+    # Initialize CXX tool from distutils info
+    initialize_cxx(env, path_list)
+
+    # Adding default tools for the one we do not customize: mingw is special
+    # according to scons, don't ask me why, but this does not work as expected
+    # for this tool.
+    if not built_with_mingw(env):
+        for i in [DEF_LINKERS, DEF_ASSEMBLERS, DEF_ARS]:
+            t = FindTool(i, env) or i[0]
+            Tool(t)(env)
+    else:
+        try:
+            t = FindTool(['g++'], env)
+            #env['LINK'] = None
+        except EnvironmentError:
+            raise RuntimeError('g++ not found: this is necessary with mingw32 '\
+                               'to build numpy !')
+        # XXX: is this really the right place ?
+        env.AppendUnique(CFLAGS = '-mno-cygwin')
+
+    # Set ALLOW_UNDEFINED link flags to allow undefined symbols in dynamic
+    # libraries
+    initialize_allow_undefined(env)
+
+    # Use our own stupid link, because we handle this with conf checkers and
+    # link flags.
+    env['SMARTLINK']   = dumb_link
+
+# Our own smart_link: the one in scons is not that smart.
+def dumb_link(source, target, env, for_signature):
+    has_cplusplus = iscplusplus(source)
+    # XXX: it would be nice to raise a warning if F77_LDFLAGS is not found in
+    # env and we link some fortran code.
+    if has_cplusplus:
+        return '$CXX'
+    return '$CC'
+
+def initialize_allow_undefined(env):
+    import SCons
+
+    if sys.platform == 'darwin':
+        env['ALLOW_UNDEFINED'] = get_darwin_allow_undefined()
+    else:
+        env['ALLOW_UNDEFINED'] = SCons.Util.CLVar('')
