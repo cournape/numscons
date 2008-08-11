d3803f9deb692feada0368f52e2a97c56714e309
david <david@evans-2.local>
2008-08-11 15:35:29 -0500

    Fix post hook example.

--- tests/examples/hook/setup.py
+++ tests/examples/hook/setup.py
@@ -5,7 +5,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('hook',parent_package,top_path)
 
-    def foo():
+    def foo(*args, **kw):
         print "scons hook test: foo"
     config.add_sconscript('SConstruct', post_hook = foo, source_files = ['hello.c'])
     return config
