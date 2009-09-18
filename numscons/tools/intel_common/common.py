3c9b49ef968c7e59028eb0bda78b1474a49339f3
David Cournapeau <cournape@gmail.com>
2009-09-18 17:50:39 +0900

    Add a language argument to get abi for intel tools.

--- numscons/tools/intel_common/common.py
+++ numscons/tools/intel_common/common.py
@@ -3,11 +3,17 @@
 
 _ARG2ABI = {'x86': 'ia32', 'amd64': 'em64t', 'default': 'ia32'}
 
-def get_abi(env):
-    try:
-        abi = env['ICC_ABI']
-    except KeyError:
-        abi = 'default'
+def get_abi(env, lang='C'):
+    if lang == 'C' or lang == 'CXX':
+        try:
+            abi = env['ICC_ABI']
+        except KeyError:
+            abi = 'default'
+    elif lang == 'FORTRAN':
+        try:
+            abi = env['IFORT_ABI']
+        except KeyError:
+            abi = 'default'
 
     try:
         return _ARG2ABI[abi]
